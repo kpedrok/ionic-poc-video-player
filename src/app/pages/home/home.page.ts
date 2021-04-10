b5e3932464f9e3318e20c5169ea12bf279a4d420
kpedrok <pedrokvb@gmail.com>
2021-04-10 16:35:38 -0300

    Add capacitor-youtube-player

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Capacitor, Plugins } from '@capacitor/core'; // Native version
+import { YoutubePlayerWeb } from 'capacitor-youtube-player'; // Web version
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterViewInit {
+  currentYear = new Date().getFullYear();
 
   constructor() {}
 
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    if (Capacitor.platform === 'web') {
+      this.initializeYoutubePlayerPluginWeb();
+    } else {
+      // Native
+      this.initializeYoutubePlayerPluginNative();
+    }
+  }
+
+  async initializeYoutubePlayerPluginWeb() {
+    const options = {
+      playerId: 'youtube-player',
+      playerSize: { width: 640, height: 360 },
+      videoId: 'tDW2C6rcH6M',
+    };
+    const result = await YoutubePlayerWeb.initialize(options);
+    console.log('playerReady', result);
+  }
+
+  async initializeYoutubePlayerPluginNative() {
+    const { YoutubePlayer } = Plugins;
+    const options = { width: 640, height: 360, videoId: 'tDW2C6rcH6M' };
+    const playerReady = await YoutubePlayer.initialize(options);
+  }
+
+  async destroyYoutubePlayerPluginWeb() {
+    const result = await YoutubePlayerWeb.destroy('youtube-player');
+    console.log('destroyYoutubePlayer', result);
+  }
 }
